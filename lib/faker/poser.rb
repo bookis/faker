0ad35edbd32e38b6dbf837e98871f850876bb230
Elise Worthy <elise.worthy@livingsocial.com>
2012-06-17 23:09:32 -0400

    downcase, fix commas and periods

--- lib/faker/poser.rb
+++ lib/faker/poser.rb
@@ -5,7 +5,7 @@ module Faker
   class Poser < Lorem
     def self.words(num = 3, type = "hipster-centric")
       hipster = HTTParty.get("http://hipsterjesus.com/api/?html=false&type=#{type}")
-      hipster_array = JSON.parse(hipster.body)["text"].gsub(/\..|,/, "").split(" ")
+      hipster_array = JSON.parse(hipster.body)["text"].downcase.gsub(/\.|,/, "").split(" ")
       hipster_array.shuffle[0, num]
     end
   end
